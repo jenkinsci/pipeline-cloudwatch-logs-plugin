012afe1d8364dc0bcf883bfd22e1586cd8c8dab9
Jesse Glick <jglick@cloudbees.com>
2018-11-19 11:16:58 -0500

    Reverting f62c139628b12b6f652b61e2b8c22372ab0ad7b7 as https://github.com/jenkinsci/aws-credentials-plugin/pull/46 has been released.

--- src/test/java/io/jenkins/plugins/pipeline_log_fluentd_cloudwatch/PipelineBridgeTest.java
+++ src/test/java/io/jenkins/plugins/pipeline_log_fluentd_cloudwatch/PipelineBridgeTest.java
@@ -24,14 +24,6 @@
 
 package io.jenkins.plugins.pipeline_log_fluentd_cloudwatch;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
-import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
-import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
 import com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
@@ -51,9 +43,7 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import jenkins.model.Jenkins;
 import jenkins.security.MasterToSlaveCallable;
-import org.apache.commons.lang.StringUtils;
 import static org.hamcrest.Matchers.*;
 import org.jenkinsci.plugins.workflow.log.LogStorage;
 import org.jenkinsci.plugins.workflow.log.LogStorageTestBase;
@@ -77,7 +67,7 @@ public class PipelineBridgeTest extends LogStorageTestBase {
         String credentialsId = null;
         if (role != null) {
             credentialsId = "aws";
-            SystemCredentialsProvider.getInstance().getCredentials().add(new PatchedAWSCredentialsImpl(CredentialsScope.GLOBAL, credentialsId, null, null, null, role, null));
+            SystemCredentialsProvider.getInstance().getCredentials().add(new AWSCredentialsImpl(CredentialsScope.GLOBAL, credentialsId, null, null, null, role, null));
             CredentialsAwsGlobalConfiguration.get().setCredentialsId(credentialsId);
         }
         CloudWatchAwsGlobalConfiguration configuration = ExtensionList.lookupSingleton(CloudWatchAwsGlobalConfiguration.class);
@@ -85,38 +75,6 @@ public class PipelineBridgeTest extends LogStorageTestBase {
         assumeThat(logGroupNameValidation.toString(), logGroupNameValidation.kind, is(FormValidation.Kind.OK));
         configuration.setLogGroupName(logGroupName);
         id = UUID.randomUUID().toString();
-    }
-
-    // TODO remove when depending on https://github.com/jenkinsci/aws-credentials-plugin/pull/46
-    private static final class PatchedAWSCredentialsImpl extends AWSCredentialsImpl {
-        PatchedAWSCredentialsImpl(CredentialsScope scope, String id, String accessKey, String secretKey, String description, String iamRoleArn, String iamMfaSerialNumber) {
-            super(scope, id, accessKey, secretKey, description, iamRoleArn, iamMfaSerialNumber);
-        }
-        @Override public AWSCredentials getCredentials() {
-            AWSCredentials initialCredentials = new BasicAWSCredentials(getAccessKey(), getSecretKey().getPlainText());
-            if (StringUtils.isBlank(getIamRoleArn())) {
-                return initialCredentials;
-            } else {
-                AWSSecurityTokenService client;
-                if (StringUtils.isBlank(getAccessKey()) && StringUtils.isBlank(getSecretKey().getPlainText())) {
-                    client = AWSSecurityTokenServiceClientBuilder.defaultClient();
-                } else {
-                    client = AWSSecurityTokenServiceClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(initialCredentials)).build();
-                }
-                AssumeRoleRequest assumeRequest = createAssumeRoleRequest(getIamRoleArn());
-                AssumeRoleResult assumeResult = client.assumeRole(assumeRequest);
-                return new BasicSessionCredentials(
-                        assumeResult.getCredentials().getAccessKeyId(),
-                        assumeResult.getCredentials().getSecretAccessKey(),
-                        assumeResult.getCredentials().getSessionToken());
-            }
-        }
-        private static AssumeRoleRequest createAssumeRoleRequest(String iamRoleArn) {
-            return new AssumeRoleRequest()
-                    .withRoleArn(iamRoleArn)
-                    .withDurationSeconds(STS_CREDENTIALS_DURATION_SECONDS)
-                    .withRoleSessionName(Jenkins.get().getDisplayName());
-        }
     }
 
     // TODO pulled up into https://github.com/jenkinsci/workflow-api-plugin/pull/83 with some modifications; move into jenkins-test-harness
